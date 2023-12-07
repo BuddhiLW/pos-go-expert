4ac8a4e0d693a01e1c82b063fa09679c65330964
BuddhiLW <pedrogbranquinho@gmail.com>
2023-12-07 19:22:34 -0300

    feat, doc: Pointers

---
+++ fundacao/pointers-variables.go
@@ -0,0 +1,36 @@
+package fundacao
+
+import "fmt"
+
+func (s *StruFoo) FM1() {
+	s.S = "passed through FM1"
+	fmt.Println("Passing through FM1, we have: i.S =", s.S)
+}
+
+func (s StruFoo) FnM2() {
+	s.S = "passed through FM2"
+	fmt.Println("Passing through FnM2, we have: i.S =", s.S)
+}
+
+func Pointers() {
+	i := StruFoo{
+		S: "Foo",
+		I: 256,
+		B: true,
+	}
+
+	fmt.Println("\n-----------------------------------\n")
+	fmt.Println("Pointers:")
+	a := 256
+	fmt.Printf("a value is: %v (a). It's adress is: %v (&a) \n", a, &a)
+
+	fmt.Println("i value is:", i)
+	// A function which Mutates
+	i.FM1()
+	fmt.Println("after i.FM1(), we have: i.S =", i.S)
+
+	// A function which does not Mutate
+	i.FnM2()
+	fmt.Println("after i.FnM2(), we have: i.S =", i.S)
+
+}
--- main.go
+++ main.go
@@ -7,4 +7,5 @@ import (
 func main() {
 	fundacao.Structs()
 	fundacao.Interfaces()
+	fundacao.Pointers()
 }
